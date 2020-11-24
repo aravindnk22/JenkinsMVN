85be2c8eaef9f98b17b39f253a356d1770f671e9
aravindnk22 <71205463+aravindnk22@users.noreply.github.com>
2020-11-24 11:46:00 +0530

    Update SampleSeleniumTest.java

--- src/test/java/com/trn/jen/SampleSeleniumTest.java
+++ src/test/java/com/trn/jen/SampleSeleniumTest.java
@@ -35,7 +35,7 @@ public class SampleSeleniumTest {
 		//System.out.println("Clicking Sign In link");
 		//driver.findElement(By.xpath("/html/body/nav/div/a[2]")).click();
 		System.out.println("Entering username");
-		driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("renju.jenkins.training");
+		driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("aravindnk22");
 		System.out.println("Clicking Next button");
 		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]//div/button/div[2]")).click();
 		Thread.sleep(5000);
